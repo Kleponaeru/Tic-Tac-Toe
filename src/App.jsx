b0c14d37031f69fc673cf2bfd43ed620043b4667
Kleponaeru <kelvinlie08@gmail.com>
2025-03-04 08:38:22 +0700

    use win combination

--- src/App.jsx
+++ src/App.jsx
@@ -31,10 +31,23 @@ function App() {
     gameBoard[row][col] = player;
   }
 
+  let winner;
+
   for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol = gameBoard[0];
-    const secondSquareSymbol = gameBoard[1];
-    const thirdSquareSymbol = gameBoard[2];
+    const firstSquareSymbol = 
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = 
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
   }
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -63,7 +76,8 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} board={gameTurns} />
+        {winner && <p>You won, {winner}!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
